1f89013127ca17b3daa8edde9579f6954d3b77cb
Dhananjay Kulkarni <dhananjaykulkarni@quickenloans.com>
2018-12-09 23:02:35 -0500

    updating tests

--- test_main.py
+++ test_main.py
@@ -8,3 +8,8 @@ class Test_main(unittest.TestCase):
     def test_main(self):
         obj = main.Main_Class("testing")
         self.assertEqual(obj.name, "testing")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
